bcad88a127e7c94d776ccfdc35d497de333f2a62
ginjaclub <ginja.club@gmail.com>
2026-03-04 14:50:33 -0800

    Update app.py Smaller Batch Size

--- app.py
+++ app.py
@@ -604,7 +604,7 @@ def transform():
         
         # Process in batches
         transformed_rows = []
-        batch_size = 5
+        batch_size = 3
         
         for i in range(0, len(rows), batch_size):
             batch = rows[i:i+batch_size]
